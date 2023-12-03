a1f6cfe6f7271d693149a90207348f1a04b87f9f
gerardaragones <gearci@fp.insjoaquimmir.cat>
2023-12-04 00:08:45 +0100

    1er commit

--- routes/web.php
+++ routes/web.php
@@ -48,18 +48,18 @@ Route::get('mail/test', [MailController::class, 'test']);
 // Files
 
 Route::resource('files', FileController::class)
-    ->middleware(['auth', 'role:' . Role::ADMIN]);
+    ->middleware('auth');
 
 Route::get('files/{file}/delete', [FileController::class, 'delete'])->name('files.delete')
-    ->middleware(['auth', 'role:' . Role::ADMIN]);
+    ->middleware('auth');
 
 // Posts
 
 Route::resource('posts', PostController::class)
-    ->middleware(['auth', 'role.any:' . implode(',', [Role::ADMIN, Role::AUTHOR])]);
+    ->middleware('auth');
 
 Route::get('posts/{post}/delete', [PostController::class, 'delete'])->name('posts.delete')
-    ->middleware(['auth', 'role.any:' . implode(',', [Role::ADMIN, Role::AUTHOR])]);
+    ->middleware('auth');
 
 Route::post('/posts/{post}/likes', [PostController::class, 'like'])->name('posts.like');
 
@@ -68,7 +68,7 @@ Route::delete('/posts/{post}/likes}', [PostController::class, 'unlike'])->name('
 // Places
 
 Route::resource('places', PlaceController::class)
-    ->middleware(['auth', 'role.any:' . implode(',', [Role::ADMIN, Role::AUTHOR])]);
+    ->middleware('auth');
 
 Route::get('places/{place}/delete', [PlaceController::class, 'delete'])->name('places.delete')
-    ->middleware(['auth', 'role.any:' . implode(',', [Role::ADMIN, Role::AUTHOR])]);
+    ->middleware('auth');
