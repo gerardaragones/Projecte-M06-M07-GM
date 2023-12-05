ad0a7265b43dce052a40a8d89517fbad1a42960f
gerardaragones <gearci@fp.insjoaquimmir.cat>
2023-12-05 18:42:35 +0100

    correccion de errores

--- app/Http/Controllers/LanguageController.php
+++ app/Http/Controllers/LanguageController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\App;
 
 class LanguageController extends Controller
 {
